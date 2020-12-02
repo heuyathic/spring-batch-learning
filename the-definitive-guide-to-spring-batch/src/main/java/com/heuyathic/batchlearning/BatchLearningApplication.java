cad96b0b9abb8bb4d4af671c6e775bc8dd1df8fd
heuyathic <heuyathic@gmail.com>
2020-12-02 20:03:01 +0900

    init commit

---
+++ the-definitive-guide-to-spring-batch/src/main/java/com/heuyathic/batchlearning/BatchLearningApplication.java
@@ -0,0 +1,13 @@
+package com.heuyathic.batchlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchLearningApplication.class, args);
+	}
+
+}
---
+++ the-definitive-guide-to-spring-batch/src/test/java/com/heuyathic/batchlearning/BatchLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heuyathic.batchlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
